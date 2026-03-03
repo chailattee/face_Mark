b5799a42221dccea7ee3215ee6f0ba4b2c935742
chailattee <charlottenorris1111@gmail.com>
2026-03-02 21:24:33 -0500

    added face mesh

--- face_detect.py
+++ face_detect.py
@@ -6,11 +6,13 @@ import time
 import mediapipe as mp
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-from mediapipe.tasks.python.vision import drawing_utils
-from mediapipe.tasks.python.vision import drawing_styles
-
 
 # primary camera was "0" 
 cap = cv2.VideoCapture(0)
+
+mp_face_mesh = mp.solutions.face_mesh
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+face_mesh = mp_face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
