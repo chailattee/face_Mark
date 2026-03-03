a5e488fc3b23b4859c10ef7aaf41c54873e04df1
chailattee <charlottenorris1111@gmail.com>
2026-03-02 19:46:33 -0500

    added facelandmarker and pngs

--- face_detect.py
+++ face_detect.py
@@ -1,6 +1,38 @@
 import cv2
-import mediapipe as mp
+import numpy as np
+import os
+import time
 
-# primary camera was "0" if you want any other source, change the index inside "VideoCapture".
+import mediapipe as mp
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe.tasks.python.vision import drawing_utils
+from mediapipe.tasks.python.vision import drawing_styles
+
+model_path = '/absolute/path/to/face_landmarker.task'
+if not os.path.exists(model_path):
+    raise FileNotFoundError(f"Model not found at: {model_path}")
+
+# primary camera was "0" 
 cap = cv2.VideoCapture(0)
 
+# mediapipe face landmarker
+BaseOptions = mp.tasks.BaseOptions
+FaceLandmarker = mp.tasks.vision.FaceLandmarker
+FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
+FaceLandmarkerResult = mp.tasks.vision.FaceLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+# Create a face landmarker instance with the live stream mode:
+def print_result(result: FaceLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print('face landmarker result: {}'.format(result))
+
+options = FaceLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path=model_path),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
+
+with FaceLandmarker.create_from_options(options) as landmarker:
+  # The landmarker is initialized. Use it here.
